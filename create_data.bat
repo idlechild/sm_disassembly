2c6de31083b6b036c97ff097b60e7cacc15660a4
InsaneFirebat <InsaneFirebat@gmail.com>
2025-01-06 06:49:17 -0500

    Fix path for data extraction

--- create_data.bat
+++ create_data.bat
@@ -2,4 +2,4 @@
 @echo off
 
 echo Extracting assets from original ROM
-python tools/rip_assets.py "../Super Metroid.sfc" -o ../data || PAUSE
+python tools/rip_assets.py "Super Metroid.sfc" -o data || PAUSE
